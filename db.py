b2e4d0f79a39ea7a1ec83732400b48064687d044
manolo-kr <dhcksdn92@gmail.com>
2025-08-25 09:30:51 +0900

    db.py 업데이트

--- db.py
+++ db.py
@@ -1,19 +1,14 @@
 # backend/app/db.py
 
 from __future__ import annotations
-
 from sqlmodel import SQLModel, create_engine, Session
+from .config import DATABASE_URL
 
-from .config import DB_URL
-
-_engine = create_engine(DB_URL, echo=False, pool_pre_ping=True)
-
+_engine = create_engine(DATABASE_URL, echo=False, future=True)
 
 def get_session() -> Session:
     with Session(_engine) as s:
         yield s
 
-
 def create_db_and_tables() -> None:
-    from . import models  # ensure models are imported
     SQLModel.metadata.create_all(_engine)
